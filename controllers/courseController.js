22a1263ed53e7c1db2635fdc65d1e73823ea0a11
Nicolas Ibarra <niicoibarra1@gmail.com>
2024-12-12 21:23:58 -0300

    Developing sorting logic

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -2,7 +2,7 @@ const OrderCourses = (req, res) => {
     try {
         req = req.body;
         let result = OrderAlgorithm(req)
-        res.send({ test: "hola" });
+        res.send({ result });
     } catch {
         throw new Error('Failed to ordering courses');
     }
@@ -15,24 +15,27 @@ function OrderAlgorithm(jsonInput) {
     const pendingCourses = [...courseList];
     
     for (let i = 0; i < pendingCourses.length; i++) {
-        console.log("llego")
         
-        for (let j = 0; j < cursosPendientes.length; j++) {
-            const { desiredCourse, requiredCourse } = cursosPendientes[i];
-            console.log(cursosPendientes[i])
-            
-        // Si el prerequisito ya está en el plan o no tiene prerequisito
-        // if (!requiredCourse || planEstudio.includes(requiredCourse)) {
-        //   planEstudio.push(desiredCourse); // Agregar el curso al plan
-        //   cursosPendientes.splice(i, 1); // Remover de los pendientes
-        //   break; // Reiniciar la búsqueda
+      // for (let j = 0; j < pendingCourses.length; j++) {
+
+        console.log(pendingCourses[i])
+        // if(pendingCourses[i].requiredCourse == pendingCourses[j].desiredCourse){
+        //   let movingCourse = pendingCourses.splice(pendingCourses[j], 1);
+        //   pendingCourses.splice(pendingCourses[i-1], 0, movingCourse);
         // }
-      }
+          
+      // Si el prerequisito ya está en el plan o no tiene prerequisito
+      // if (!requiredCourse || planEstudio.includes(requiredCourse)) {
+      //   planEstudio.push(desiredCourse); // Agregar el curso al plan
+      //   cursosPendientes.splice(i, 1); // Remover de los pendientes
+      //   break; // Reiniciar la búsqueda
+      // }
+      // }
     }
   
     return {
       userId,
-      orderedCourses: planEstudio,
+      pendingCourses,
     };
   }
 
