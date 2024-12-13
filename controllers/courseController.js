18b2f0e39fafc3ed82e6d56e89a86ce2d5bb70d4
Nicolas Ibarra <niicoibarra1@gmail.com>
2024-12-13 01:37:08 -0300

    Developing sorting logic

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,42 +1,47 @@
 const OrderCourses = (req, res) => {
     try {
         req = req.body;
-        let result = OrderAlgorithm(req)
-        res.send({ result });
+        let sortedList = OrderAlgorithm(req.courseList)
+        res.send({ sortedList });
     } catch {
         throw new Error('Failed to ordering courses');
     }
 }
 
-function OrderAlgorithm(jsonInput) {
-    const { userId, courseList } = jsonInput;
-
-    const studyPlan = [];
-    const pendingCourses = [...courseList];
-    
-    for (let i = 0; i < pendingCourses.length; i++) {
-        
-      // for (let j = 0; j < pendingCourses.length; j++) {
-
-        console.log(pendingCourses[i])
-        // if(pendingCourses[i].requiredCourse == pendingCourses[j].desiredCourse){
-        //   let movingCourse = pendingCourses.splice(pendingCourses[j], 1);
-        //   pendingCourses.splice(pendingCourses[i-1], 0, movingCourse);
-        // }
-          
-      // Si el prerequisito ya está en el plan o no tiene prerequisito
-      // if (!requiredCourse || planEstudio.includes(requiredCourse)) {
-      //   planEstudio.push(desiredCourse); // Agregar el curso al plan
-      //   cursosPendientes.splice(i, 1); // Remover de los pendientes
-      //   break; // Reiniciar la búsqueda
-      // }
-      // }
-    }
+function GetRootCourses(courses) {
+  let notDesiredCourses = [];
   
-    return {
-      userId,
-      pendingCourses,
-    };
+  for(let i = 0; i < courses.length; i++){
+    let isDesired = false;
+
+    for(let j = 0; j < courses.length; j++){
+      if(courses[i].requiredCourse == courses[j].desiredCourse){
+        isDesired = true;
+      }
+    }
+
+    if(!isDesired && !notDesiredCourses.some(c => c == courses[i].requiredCourse)){
+      notDesiredCourses.push(courses[i]);
+    }
   }
+  return notDesiredCourses;
+}
+
+function GetNextCourses(rootCourses, pendingCourses, arePendingCourses){
+  arePendingCourses = false;
+  return rootCourses;
+}
+
+function OrderAlgorithm(courses) {
+  let rootCourses = GetRootCourses(courses);
+
+  let sortedList;
+  let arePendingCourses = true;
+  while(arePendingCourses) {
+    sortedList = GetNextCourses(rootCourses, courses, arePendingCourses);
+  }
+
+  return sortedList;
+}
 
 module.exports = { OrderCourses }
